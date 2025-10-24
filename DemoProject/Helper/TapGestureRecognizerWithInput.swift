b20db69f664f3ca97516489f5b091ebe3dda93d0
Rashad359 <aliyevrasad07@gmail.com>
2025-10-24 12:21:21 +0400

    various changes and improvements

---
+++ DemoProject/Helper/TapGestureRecognizerWithInput.swift
@@ -0,0 +1,12 @@
+//
+//  TapGestureRecognizerWithInput.swift
+//  DemoProject
+//
+//  Created by Rəşad Əliyev on 10/24/25.
+//
+
+import UIKit
+
+class StringGestureRecognizer: UITapGestureRecognizer {
+    var input: String?
+}
